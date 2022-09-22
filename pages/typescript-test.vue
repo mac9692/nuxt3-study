a67ebf32d31e860a43feb8e2db1eba6ca9d38d94
1328384 <mac9692@nate.com>
2022-09-22 17:56:44 +0900

    20220922 nuxt3+typescript study

--- pages/typescript-test.vue
+++ pages/typescript-test.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
     <h1>typescript-test</h1>
-    <h1>{{typepark}}</h1>
+    <h1>{{park}}</h1>
     <button @click="magic">마법의 버튼</button>
+    <h1>{{sum(5,2)}}</h1>
   </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "typescript-test",
-  data() {
-    return {
-      typepark: 'This is data() TypeTest',
-    }
+
+  data: {
+      park: 'This is data() TypeTest',
   },
 
-
   methods: {
-    magic() {
-      this.typepark = 'magic Language';
+    magic(): void {
+      this.park = 'magic Language';
+    },
+
+    sum(a: number, b: number): number {
+      return a + b;
     }
   }
 }
+
 </script>
 
 <style scoped>
